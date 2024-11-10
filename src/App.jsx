6e7164933a0ec5fb14191c4b38f25eae78388ce9
dorra13 <149484893+dorra13@users.noreply.github.com>
2024-11-10 16:23:05 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 // App.jsx
 
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Import Router and Routes
 
 import HomePage from "./pages/HomePage";
 import CreatePage from "./pages/CreatePage";
+import EditPostPage from "./pages/EditPostPage";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/create" element={<CreatePage />} />
-    </Routes>
+    <Router>
+      {" "}
+      {/* Router is wrapped only here */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/edit-post/:id" element={<EditPostPage />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
-
