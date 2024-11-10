59ba395922404af890c74ffc93e56cf93ccbe7b9
dorra13 <149484893+dorra13@users.noreply.github.com>
2024-11-10 15:13:07 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App /> {/* No need for BrowserRouter here */}
+   <BrowserRouter basename={import.meta.env.DEV ? "/" : "/react-web-app/"}>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
