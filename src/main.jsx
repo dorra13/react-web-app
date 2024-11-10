5a50aba030d1bdefe4a9e2b56c4a4b76f72cd1c3
dorra13 <149484893+dorra13@users.noreply.github.com>
2024-11-10 15:20:13 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-   <BrowserRouter basename={import.meta.env.DEV ? "/" : "/react-web-app/"}>
+    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/react-web-app/"}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
