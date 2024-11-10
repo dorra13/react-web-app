d8af8ed9555e46303e63703c27d040db5573c8e6
dorra13 <149484893+dorra13@users.noreply.github.com>
2024-11-10 16:31:19 +0100

    fixing routing issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // App.jsx
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Import Router and Routes
+import { Routes, Route } from "react-router-dom"; // Import Router and Routes
 
 import HomePage from "./pages/HomePage";
 import CreatePage from "./pages/CreatePage";
@@ -8,15 +8,11 @@ import EditPostPage from "./pages/EditPostPage";
 
 function App() {
   return (
-    <Router>
-      {" "}
-      {/* Router is wrapped only here */}
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
         <Route path="/edit-post/:id" element={<EditPostPage />} />
       </Routes>
-    </Router>
   );
 }
 
